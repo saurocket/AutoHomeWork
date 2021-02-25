f8759d036a721a04802f6fcd4f93a29dc174f8d0
Yauheni Luzakou <37931774+saurocket@users.noreply.github.com>
2021-02-25 12:01:05 +0100

    success4

--- module_4.js
+++ module_4.js
@@ -1,36 +1,165 @@
-const atTheOldToad = {
-  potions: [
-    { name: 'Зелье скорости', price: 460 },
-    { name: 'Дыхание дракона', price: 780 },
-    { name: 'Каменная кожа', price: 520 },
-  ],
-  // Пиши код ниже этой строки
-  getPotions() {
-    return this.potions;
-  },
-  addPotion(potionName) {
+//1-10
+function makePizza() {
+  return 'Ваша пицца готовится, ожидайте.';
+}
+// Пиши код ниже этой строки
 
-    this.potions.map(u => {
-      if (u.name ===potionName.name){
-        return `Зелье ${potionName} уже есть в инвентаре!`;
-      }
-    })
-    this.potions.push(potionName);
-  },
-  removePotion(potionName) {
-    for (let i = 0; i < this.potions.length; i++){
-      let tempName = this.potions[i].name
-   
-      if (tempName === potionName){
-          this.potions.splice(i,1);
-      }
+const result = makePizza();
+const pointer = makePizza;
+
+//2-10
+function deliverPizza(pizzaName) {
+  return `Доставляем пиццу ${pizzaName}.`;
+}
+
+function makePizza(pizzaName) {
+  return `Пицца ${pizzaName} готовится, ожидайте...`;
+}
+
+// Пиши код ниже этой строки
+function makeMessage(pizzaName,  callback) {
+  return callback(pizzaName)
+}
+//3-10
+function makePizza(pizzaName, callback) {
+  console.log(`Пицца ${pizzaName} готовится, ожидайте...`);
+  callback(pizzaName);
+}
+
+makePizza('Роял гранд', function deliverPizza(pizzaName) {
+  console.log(`Доставляем пиццу ${pizzaName}.`);
+});
+// Пиши код ниже этой строки
+
+makePizza('Ультрасыр', function eatPizza(pizzaName){
+  console.log(`Едим пиццу ${pizzaName}`)
+});
+//4-10
+const pizzaPalace = {
+  pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+  order(pizzaName, pizza, error) {
+    
+    if (!this.pizzas.includes(pizzaName)){
+      return(error(`В ассортименте нет пиццы с названием ${pizzaName}.`)); 
+    }else{
+      return(pizza(pizzaName)) ;
     }
-  },
-  updatePotionName(oldName, newName) {
-    for (let i = 0; i < this.potions.length; i++){
-      if (this.potions[i].name === oldName){
-        this.potions[i].name = newName;
-      }
-    }
+
   },
 };
+
+function makePizza(pizzaName) {
+  return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+}
+
+function onOrderError(error) {
+  return `Ошибка! ${error}`;
+}
+
+console.log(pizzaPalace.order('Аль Копчино', makePizza, onOrderError));
+console.log(pizzaPalace.order('Четыре нарезона', makePizza, onOrderError));
+console.log(pizzaPalace.order('Биг майк', makePizza, onOrderError));
+console.log(pizzaPalace.order('Венская', makePizza, onOrderError));
+
+//5-10
+const pizzaPalace = {
+  pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+  // Пиши код ниже этой строки
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `В ассортименте нет пиццы с названием «${pizzaName}».`;
+    }
+
+    return `Заказ принят, готовим пиццу «${pizzaName}».`;
+  },
+  // Пиши код выше этой строки
+};
+//6-10
+const customer = {
+  username: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['Burger', 'Pizza', 'Salad'],
+  // Пиши код ниже этой строки
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Пиши код выше этой строки
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, 'Steak');
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ['Burger', 'Pizza', 'Salad', 'Steak']
+
+
+//7-10
+const orders = [
+  { email: 'solomon@topmail.ua', dish: 'Burger' },
+  { email: 'artemis@coldmail.net', dish: 'Pizza' },
+  { email: 'jacob@mail.com', dish: 'Taco' },
+];
+
+// Пиши код ниже этой строки
+function composeMessage(position) {
+return  `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.` 
+}
+const messages = orders.map((obj, i) => composeMessage.call(obj, i+1));
+console.log(messages);
+//8-10
+const orders = [
+  { email: 'solomon@topmail.ua', dish: 'Burger' },
+  { email: 'artemis@coldmail.net', dish: 'Pizza' },
+  { email: 'jacob@mail.com', dish: 'Taco' },
+];
+
+// Пиши код ниже этой строки
+function composeMessage(position) {
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+}
+
+const messages = orders.map((order, index) =>
+  composeMessage.apply(order, [index + 1])
+);
+//9-10
+const pizzaPalace = {
+  company: 'Pizza Palace',
+};
+
+const burgerShack = {
+  company: 'Burger Shack',
+};
+
+function composeMessage(customerName) {
+  return `${customerName}, всегда рады вас видеть в «${this.company}».`;
+}
+// Пиши код ниже этой строки
+
+const pizzaPalaceComposer = composeMessage.bind(pizzaPalace);
+const pizzaPalaceMessage = pizzaPalaceComposer('Манго');
+
+const burgerShackComposer = composeMessage.bind(burgerShack);
+const burgerShackMessage = burgerShackComposer('Поли');
+//10-10
+
+
+
